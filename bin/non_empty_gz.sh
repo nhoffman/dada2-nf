78e9f083038c021ee516f864d112d5cc00107502
Hoogestraat <dhoogest@gattaca.amc.uwmedicine.org>
2023-02-13 13:24:19 -0800

    little cleaner logic in bash script

--- bin/non_empty_gz.sh
+++ bin/non_empty_gz.sh
@@ -3,9 +3,7 @@
 for file in "$@"
 do
     # https://unix.stackexchange.com/a/6771
-    if LC_ALL=C gzip -l "$file" | awk 'NR==2 {exit($2!=0)}'; then
-	:
-    else
+    if LC_ALL=C gzip -l "$file" | awk 'NR==2 {exit($2==0)}'; then
 	echo "$file"
     fi
 done
